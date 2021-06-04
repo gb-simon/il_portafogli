73a9c9e1626385872187d9796828da9174f3629a
Gonzalo Simon <aguilarsimon01@gmail.com>
2021-06-04 16:17:32 -0300

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { AppComponent } from './app.component';
 import { ActionsPesosComponent } from './actions-pesos/actions-pesos.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { RegisterComponent } from './register/register.component';
 import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
-  declarations: [AppComponent, ActionsPesosComponent],
+  declarations: [
+    AppComponent,
+    ActionsPesosComponent,
+    RegisterComponent,
+    LogInComponent,
+  ],
 
   imports: [
     BrowserModule,
     NoopAnimationsModule,
     MatSliderModule,
     MatFormFieldModule,
-    RegisterComponent,
-    LogInComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
