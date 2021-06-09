d7575027b05d47e647968401767e5f6b0efe371b
Gonzalo Simon <aguilarsimon01@gmail.com>
2021-06-09 10:04:20 -0300

    I removed angular-material and installed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ActionsPesosComponent } from './actions-pesos/actions-pesos.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { RegisterComponent } from './register/register.component';
 import { LogInComponent } from './log-in/log-in.component';
-
-import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -21,9 +17,6 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     BrowserModule,
     NoopAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
